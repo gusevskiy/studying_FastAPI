da90eea1a728ee6357922bad821acc24e6f0cf6b
gusevskiy <vlgu@mail.ru>
2023-06-23 00:03:19 +0300

    added a lot

---
+++ src/operations/router.py
@@ -0,0 +1,32 @@
+from database import get_async_session
+from fastapi import APIRouter, Depends
+from operations.models import operation
+from operations.schemas import OperationCreate
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operation"]
+)
+
+
+@router.get("/")
+async def get_specific_operation(
+    operation_type: str, session: AsyncSession = Depends(get_async_session)
+):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    
+    return result.all()
+
+
+@router.post("/")
+async def add_specific_operation(
+    new_operation: OperationCreate,
+    session: AsyncSession = Depends(get_async_session)
+):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}

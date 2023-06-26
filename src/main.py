d492e901c0ed5ca26b81d905d23a4ed5260c146f
gusevskiy <vlgu@mail.ru>
2023-06-26 10:16:44 +0300

    CORS and middleware

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-
 from redis import asyncio as aioredis
-from auth.base_config import auth_backend, fastapi_users
-from auth.schemas import UserRead, UserCreate
+from fastapi.middleware.cors import CORSMiddleware
 
+from auth.base_config import auth_backend, fastapi_users
+from auth.schemas import UserCreate, UserRead
 from operations.router import router as router_operation
+from tasks.router import router as router_tasks
 
 app = FastAPI(
     title="Trading App"
@@ -25,9 +26,23 @@ app.include_router(
 )
 
 app.include_router(router_operation)
+app.include_router(router_tasks)
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
-async def startup():
-    redis = aioredis.from_url("redis://localhost")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import insert, select
 
-from src.auth.models import role
+from auth.models import role
 from conftest import client, async_session_maker
 
 
@@ -13,22 +13,17 @@ async def test_add_role():
 
         query = select(role)
         result = await session.execute(query)
-        print(result.all())
-        # assert result.all() == [(1, 'admin', None)], "Роль не добавилась"
-
-# def test_register():
-#     response = client.post("/auth/register", json={
-#         "email": "string",
-#         "password": "string",
-#         "is_active": True,
-#         "is_superuser": False,
-#         "is_verified": False,
-#         "username": "string",
-#         "role_id": 1
-#     })
-
-#     assert response.status_code == 201
-
+        assert result.all() == [(1, 'admin', None)], "Роль не добавилась"
 
 def test_register():
-    assert 1 == 1
+    response = client.post("/auth/register", json={
+        "email": "string",
+        "password": "string",
+        "is_active": True,
+        "is_superuser": False,
+        "is_verified": False,
+        "username": "string",
+        "role_id": 1
+    })
+
+    assert response.status_code == 201
